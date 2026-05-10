097485089c2460e68d88cac7937784bfa04793b8
nishadabhay18 <abhayn927@gmail.com>
2026-05-10 17:08:44 +0530

    init commit

---
+++ src/100 Same Tree/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // TC-> O(n) SC-> O(n)
+    public boolean isSameTree(TreeNode root1, TreeNode root2){
+        if(root1 == null && root2 == null) return true;
+        if(root1 == null || root2 == null) return false;
+        if(root1.val != root2.val) return false;
+        if(isSameTree(root1.left, root2.left) == false) return false;
+        if(isSameTree(root1.right, root2.right) == false) return false;
+        return true;
+    }
+}
